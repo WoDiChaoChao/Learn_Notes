d8c6e67ae94f456fd5cdd1be2d0ac474af259e74
rui <3037580090@qq.com>
2024-03-24 10:05:12 +0800

    2024/3/24补充构造函数的分类

--- 对象的初始化和清理/构造函数的分类和调用.cpp
+++ 对象的初始化和清理/构造函数的分类和调用.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 class Person
 {
-//分类
+	//分类
 public:
 	//无参构造函数(默认构造函数)
 	Person() {
 		cout << "Person 无参构造函数" << endl;
 	}
-	//有参构造函数
+	//有参构造函数(一般构造函数)
 	Person(int a) {
 		cout << "Person 有参构造函数" << endl;
 	}
@@ -21,10 +21,24 @@ public:
 	Person(const Person& p) {
 		cout << "Person 拷贝构造函数" << endl;
 	}
+	//赋值构造函数
+	Person& operator= (const Person& p) {
+		if (p._data == this->_data)
+			return *this;
+		this->_data = p._data;
+		cout << "Person 赋值构造函数" << endl;
+		return *this;
+	}
+	//移动构造函数
+	Person(Person&& other) {
+		this->_data = other._data;
+		cout << "Person 移动构造函数" << endl;
+	}
 	~Person() {
 		cout << "Person 析构函数的调用" << endl;
 	}
 
+	int _data;
 private:
 
 };
@@ -43,6 +57,19 @@ void test01() {
 	//3.隐式转换法
 	Person p1 = 10;
 	Person p2 = p1;
+
+	//4.调用赋值构造函数
+	Person p3;
+	p3._data = 10;
+	Person p4;
+	p4 = p3;
+	cout << p4._data << endl;
+
+	//5.调用移动构造函数
+	Person p5;
+	p5._data = 10;
+	Person p6 = std::move(p5);
+	cout << p6._data << endl;
 }
 
 int main() {
